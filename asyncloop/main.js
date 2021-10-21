5b389b45dfe617fea33635f7f352db295d7c7b7b
sina42048 <sinafat76@gmail.com>
2021-10-21 11:15:13 +0330

    behind the scene of async await

---
+++ asyncloop/main.js
@@ -0,0 +1,58 @@
+const delay = (timeout) => new Promise((res, rej) => setTimeout(res, timeout));
+
+// (async function () {
+//   for (let i = 0; i < 10; i++) {
+//     console.log(i);
+//     await delay(2000);
+//   }
+// })();
+
+// is equivalent to
+
+function asyncLoop(generator) {
+  const gen = generator();
+  generatorRunner(gen);
+}
+
+function isThenable(fn) {
+  return typeof fn.then === "function";
+}
+
+function generatorRunner(generator) {
+  const firsStep = generator.next();
+  const step = {
+    next: () => generator.next(),
+    current: firsStep,
+  };
+  timeLoop(step);
+}
+
+function timeLoop(step) {
+  const timerLoop = setTimeout(() => {
+    if (step.current.value === void 0) {
+      clearTimeout(timeLoop);
+      return;
+    }
+    if (isThenable(step.current.value)) {
+      step.current.value.then(() => {
+        const next = step.next();
+        step.current = next;
+        clearTimeout(timerLoop);
+        timeLoop(step);
+      });
+    } else {
+      const next = step.next();
+      step.current = next;
+      clearTimeout(timerLoop);
+      timeLoop(step);
+    }
+  }, 0);
+}
+
+asyncLoop(function* () {
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+    yield delay(2000);
+    yield "test"; // not promise
+  }
+});
